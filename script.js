a19e3d9a61008260a67d899a540c82646a0ad05f
Priaa_Sigmaa <ragilkaela23@gmail.com>
2024-10-28 22:25:40 +0700

    Files

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiText", {
+    strings: ["Web Development", "Youtuber", "Programmer", "&", "Gamer"],
+    typeSpeed: 100,
+    backSpeed: 80, // Corrected property name
+    backDelay: 1500,
+    loop: true
+})
